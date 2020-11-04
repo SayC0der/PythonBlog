d5beaa1417a68a25d42e19c081262ac58570a4c5
Achraf Marbati <64266975+SayC0der@users.noreply.github.com>
2020-11-04 23:23:40 +0100

    Update settings.py

--- Blognow/settings.py
+++ Blognow/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')m_9@+t%_v=#)1gv1dg%1u^6#hl(u&(ee(css3w5vf5gofjba-'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
